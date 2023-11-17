7f8db2c8a3bb870070668314b819b6b1ecd04688
kibati12345 <109470159+kibati12345@users.noreply.github.com>
2023-11-16 20:00:31 -0500

    addinf 2 if statements

--- Assignment6/main.js
+++ Assignment6/main.js
@@ -25,7 +25,18 @@ function result() {
     newStory = newStory.replaceAll(':inserty:',yItem);
     newStory = newStory.replaceAll(':insertz:',zItem);
 
-    
+
+    if (customName.value !== '') {
+        const name = customName.value;
+        newStory = newStory.replaceAll('Bob', name);
+      }
+
+    if (document.getElementById("uk").checked) {
+        const weight = `${Math.round(300*0.0714286)} stone`;
+        const temperature =  `${Math.round((94-32) * 5 / 9)} centigrade`;
+        newStory = newStory.replaceAll('94 fahrenheit', temperature);
+        newStory = newStory.replaceAll('300 pounds', weight);
+    }
 }
 
 
