c934474db51584dddd8e29db0d4c7e647c761cb2
kibati12345 <109470159+kibati12345@users.noreply.github.com>
2023-11-24 16:52:55 -0500

    added the collision detect function

--- Assignment6/main-finished.js
+++ Assignment6/main-finished.js
@@ -136,6 +136,26 @@ checkBounds()
    }
 }
 
+collisionDetect()
+{
+   for (const ball of balls) {
+     if (ball.exists) {
+       const dx = this.x - ball.x;
+       const dy = this.y - ball.y;
+       const distance = Math.sqrt(dx * dx + dy * dy);
+
+       if (distance < this.size + ball.size) {
+         ball.exists = false;
+         count--;
+         para.textContent = 'Ball count: ' + count;
+       }
+     }
+   }
+}
+
+
+
+
 
 const balls = [];
 
