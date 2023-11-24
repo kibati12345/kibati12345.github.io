fa692c4376a931dc4942b20cc3c759cde353e05f
kibati12345 <109470159+kibati12345@users.noreply.github.com>
2023-11-24 16:35:00 -0500

    added extended shape

--- Assignment6/main-finished.js
+++ Assignment6/main-finished.js
@@ -29,17 +29,15 @@ class Shape {
    }
  
  }
-class Ball {
+class Ball extends Shape {
 
    constructor(x, y, velX, velY, color, size) {
-      this.x = x;
-      this.y = y;
-      this.velX = velX;
-      this.velY = velY;
+      super(x, y, velX, velY);
+  
       this.color = color;
       this.size = size;
-   }
-
+      this.exists = true;
+    }
    draw() {
       ctx.beginPath();
       ctx.fillStyle = this.color;
