732e4c7b64c86df3b4e2c6e376d26bd4d97ec90e
kibati12345 <109470159+kibati12345@users.noreply.github.com>
2023-11-24 16:59:51 -0500

    calling the function for evil ball

--- Assignment6/main-finished.js
+++ Assignment6/main-finished.js
@@ -191,7 +191,10 @@ function loop() {
          ball.collisionDetect();
       }
    }
-
+   evilBall.draw();
+   evilBall.checkBounds();
+   evilBall.collisionDetect();
+ 
    requestAnimationFrame(loop);
 }
 
