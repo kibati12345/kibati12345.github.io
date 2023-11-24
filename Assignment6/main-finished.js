dbb1e3cdb6d5ecfe43a619964fa63b4cf16c3313
kibati12345 <109470159+kibati12345@users.noreply.github.com>
2023-11-24 16:56:56 -0500

    added an if statement

--- Assignment6/main-finished.js
+++ Assignment6/main-finished.js
@@ -184,9 +184,12 @@ function loop() {
    ctx.fillRect(0, 0,  width, height);
 
    for (const ball of balls) {
-     ball.draw();
-     ball.update();
-     ball.collisionDetect();
+      if (ball.exists) 
+      {
+         ball.draw();
+         ball.update();
+         ball.collisionDetect();
+      }
    }
 
    requestAnimationFrame(loop);
