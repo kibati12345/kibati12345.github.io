b9dc0f382252a2d6d9d2f40bcf99e3211c5bd715
kibati12345 <109470159+kibati12345@users.noreply.github.com>
2023-11-24 16:55:05 -0500

    constant evil ball

--- Assignment6/main-finished.js
+++ Assignment6/main-finished.js
@@ -177,6 +177,8 @@ while (balls.length < 25) {
   para.textContent = 'Ball count: ' + count;
 }
 
+const evilBall = new EvilCircle(random(0, width), random(0, height));
+
 function loop() {
    ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
    ctx.fillRect(0, 0,  width, height);
