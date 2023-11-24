3c2ef27c1ff70f578405a0ef2d935f37a95504f0
kibati12345 <109470159+kibati12345@users.noreply.github.com>
2023-11-24 16:08:27 -0500

    added an else statement

--- Assignment6/gallery-start 2/main.js
+++ Assignment6/gallery-start 2/main.js
@@ -35,7 +35,11 @@ btn.addEventListener('click', () => {
       btn.setAttribute('class','light');
       btn.textContent = 'Lighten';
       overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
-    } 
+    } else {
+        btn.setAttribute('class','dark');
+        btn.textContent = 'Darken';
+        overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+      }
     }
   );
   
